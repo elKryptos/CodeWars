c13977163f449936e0e91c37bde5aeec9839fa83
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-08-31 16:47:51 +0200

    new method

--- src/Methods/Kata.java
+++ src/Methods/Kata.java
@@ -1,6 +1,7 @@
 package Methods;
 
 import java.math.BigInteger;
+import java.nio.charset.CharacterCodingException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -148,7 +149,29 @@ public class Kata {
         } else {
             return a + b;
         }
-       }
+    }
+
+    // ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot 
+    // contain anything but exactly 4 digits or exactly 6 digits. 
+    // If the function is passed a valid PIN string, return true, else return false.
+    public static boolean validatePin(String pin) {
+        if(pin.length() != 4 && pin.length() != 6) {
+            return false;
+        }
+
+        for( char c : pin.toCharArray() ) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
+    
+    
+
+
+
 
 
