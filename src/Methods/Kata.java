c6f7fa26fb6f2478cffdc4ac34c8b9d96d55ccd8
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-08-31 12:12:29 +0200

    '

--- src/Methods/Kata.java
+++ src/Methods/Kata.java
@@ -139,7 +139,16 @@ public class Kata {
         }
         return Integer.parseInt(result.toString());
     }
-    
+    // Given two integers a and b, which can be positive or negative, 
+    // find the sum of all the integers between and including them 
+    // and return it. If the two numbers are equal return a or b.
+    public int GetSum(int a, int b) {
+        if( a == b) { 
+            return a;
+        } else {
+            return a + b;
+        }
+       }
 }
 
 
