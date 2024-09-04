82bdcbdc11d5a4ea75f89827d52001b59a183fd9
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-09-04 23:34:16 +0200

    array method

--- src/Methods/Kata.java
+++ src/Methods/Kata.java
@@ -270,4 +270,15 @@ public class Kata {
         }
         return result;
     }
+
+    // Create a function with two arguments that will return an array of the first n multiples of x.
+    // Assume both the given number and the number of times to count will be positive numbers greater than 0.
+    // countBy(1,10)  // should return  {1,2,3,4,5,6,7,8,9,10}
+    public static int[] countBy(int x, int n){
+        int result [] = new int[n];
+        for (int i = 0; i < n; i++){
+            result[i] += x * (i + 1);
+        }
+        return result;
+      }
 }
