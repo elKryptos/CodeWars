025d73a4d8f6ff4d397269ebea7141e96e908dd8
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-09-09 22:06:57 +0200

    new file methods

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
 
-import Methods.KingCompany;
+import Methods.CodeGrind;
 
 public class App {
     public static void main(String[] args) {
         int[] array = {1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54};
-        System.out.println(Arrays.toString(KingCompany.getMaxFrequenti(array, 3)));
+        int f = CodeGrind.somma(array);
+        System.out.println(f);
     }
 }
---
+++ src/Methods/CodeGrind.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class CodeGrind {
+
+    // Somma degli elementi di un array di interi
+    public static int somma (int [] array){
+        int result = 0;
+        for (int value : array){
+            result += value;
+        }
+        return result;
+    } 
+
+    // Trovare il valore massimo in un array di numeri interi
+}
