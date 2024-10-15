77a81c8ad179d7608e884311b4d9fe70b37223f2
elKryptos <you@example.com>
2024-10-15 15:56:52 +0200

    added some method of work

--- src/Methods/Kata.java
+++ src/Methods/Kata.java
@@ -1,5 +1,6 @@
 package Methods;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -320,4 +321,82 @@ public class Kata {
         }
         return result.toString();
     }
+
+     public int sumOfAllElementsPresentsInGivenArray(int[] input) {
+        int result = 0;
+        for(int i = 0; i < input.length; i++){
+            result += input[i];
+        }
+        return result;
+    }
+
+    public int findMax(int[] input) {
+        int x = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] > x) {
+                x = input[i];
+            }
+        }
+        return x;
+    }
+
+    public int[][] transpose(int[][] input) {
+        int rows = input.length;
+        int columns = input[0].length;
+        int [][] trasposed = new int[rows][columns];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                trasposed[j][i] = input[i][j];
+            }
+        }
+        return trasposed;
+    }
+
+    public int[] removeDuplicate(int[] input) {
+       if(input.length == 0) return new int[0];
+       if(input.length == 1) return input;
+       Arrays.sort(input);
+       int[] temp = new int[input.length];
+       int value = 0;
+       temp[value++] = input[0];
+       for (int i = 1; i < input.length; i++) {
+           if(input[i] != temp[value - 1]) {
+               temp[value++] = input[i];
+           }
+       }
+       return Arrays.copyOf(temp, value);
+    }
+
+    public int[] removeAllOccurrences(int[] input) {
+        return new int [0];
+    }
+
+    public int[] removeElementsFromArray(int[] input, int toRemove) {
+        int count = 0;
+        for(int i = 0; i < input.length; i++){
+            if(input[i] != toRemove){
+                count++;
+            }
+        }
+        int [] result = new int[count];
+        int index = 0;
+        for(int i = 0; i < input.length; i++){
+            if(input[i] != toRemove){
+                result[index++] = input[i];
+            }
+        }
+        return result;
+    }
+
+    public String remove0FromGivenString(String input) {
+        if(input == null || input.length() == 0) return "";
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) != '0'){
+                result.append(input.charAt(i));
+            }
+        }
+        return result.toString();
+    }
 }
+
