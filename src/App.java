322d73609e8e600cb44746349c5120acf5b93179
Hans De la Cruz <hans_09@hotmail.it>
2025-02-01 20:19:05 +0100

    added new methods

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
+import java.util.*;
+
+import Methods.Kata;
+
 public class App {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 4, 5};
+        System.out.println(Kata.finder(numbers));
+        String text = "hans";
+        System.out.println(Kata.reverse(text) + "1");
+        System.out.println(Kata.reverse2(text) + "2");
+
     }
 }
