5d934e96c2550de155e8213fa03b603f41c60e1c
Hans De la Cruz <hans_09@hotmail.it>
2025-02-01 17:04:15 +0100

    gitignore

--- src/App.java
+++ src/App.java
@@ -1,12 +1,7 @@
-import Methods.CodeGrind;
+
 
 public class App {
     public static void main(String[] args) {
-        int[] array = {1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54};
-        int f = CodeGrind.minNum(array);
-        System.out.println(f);
 
-        String testo = "Hans";
-        System.out.println(CodeGrind.inverse(testo).toString());
     }
 }
